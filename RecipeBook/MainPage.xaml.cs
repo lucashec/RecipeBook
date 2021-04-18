22d0aa70cd37feb423922687106f530ace7f2433
unknown <lucashenriqueev@gmail.com>
2021-04-18 10:28:13 -0300

    registerPage

--- RecipeBook/MainPage.xaml.cs
+++ RecipeBook/MainPage.xaml.cs
@@ -31,5 +31,9 @@ namespace RecipeBook
             }
         }
 
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
+        }
     }
 }
